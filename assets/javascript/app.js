48fef0d9c54f9cdfaaed009d4c6493e7e1ab9691
David Steger <d.steger@ymail.com>
2019-04-27 20:33:19 -0700

    html layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+$(documents).ready(function() {
+
+    // Variables used for page
+    var timeLeft = 180;
+    var timer;
+    var rightAnswers = 0;
+    var wrongAnswers = 0;
+
+    // The page will load the start button but will hide the time, questions, scoreboard, done button
+    $"#timeClock").hide();
+    
+})
